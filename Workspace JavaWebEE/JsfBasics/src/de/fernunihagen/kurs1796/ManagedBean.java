0bea6cff766f3e6f1028fc54185061b3997b7913
Erik Barz <erik.barz90@gmail.com>
2019-01-17 16:26:10 +0100

    Ergänzung Aufgabe 5.2

--- Workspace JavaWebEE/JsfBasics/src/de/fernunihagen/kurs1796/ManagedBean.java
+++ Workspace JavaWebEE/JsfBasics/src/de/fernunihagen/kurs1796/ManagedBean.java
@@ -13,6 +13,12 @@ public class ManagedBean {
     // Deklaration der Properties
     private String vorname;
     private String nachname;
+    private String alter;
+    private String plz;
+    private String strasse;
+    private String mail;
+    private String gewicht;
+    private String ort;
     
     private String name;
 
@@ -41,9 +47,56 @@ public class ManagedBean {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-    public void onButtonClicked() {
+		
+    public String getAlter() {
+		return alter;
+	}
+
+	public void setAlter(String alter) {
+		this.alter = alter;
+	}
+
+	public String getPlz() {
+		return plz;
+	}
+
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getGewicht() {
+		return gewicht;
+	}
+
+	public void setGewicht(String gewicht) {
+		this.gewicht = gewicht;
+	}
+
+	public String getOrt() {
+		return ort;
+	}
+
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+
+	public void onButtonClicked() {
     	// Der zusammengesetzte Name wird erzeugt
         setName(vorname + " " + nachname);
         
